b9743b263a1a3989de1ff93d1225097717eba101
raghu <raghu.bogimi@gmail.com>
2016-08-14 18:39:58 -0500

    Annotation-Based Aspects removed xml configurations.

--- src/main/java/com/caveofprogramming/spring/aop/Camera.java
+++ src/main/java/com/caveofprogramming/spring/aop/Camera.java
@@ -1,5 +1,10 @@
 package com.caveofprogramming.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class Camera {
 	public void snap() {
 		System.out.println("SNAP!");
